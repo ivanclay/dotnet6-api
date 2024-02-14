5bffacb43c85d53f4222ec7b93348defb69158db
Ivan Clay <ivan.clay.moura@gmail.com>
2024-02-14 14:09:57 -0300

    feat: extract classes

---
+++ NET6-WEB-API/NET6.WEB/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext 
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+    {
+        
+    }
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>()
+            .Property(x => x.Description).HasMaxLength(500).IsRequired(false);
+        modelBuilder.Entity<Product>()
+            .Property(x => x.Name).HasMaxLength(120).IsRequired();
+        modelBuilder.Entity<Product>()
+            .Property(x => x.Code).HasMaxLength(20).IsRequired(false);
+    }
+
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+    //    => optionsBuilder.UseSqlServer();
+    //"Server=localhost;Database=Products;User Id=sa;Password=@sql2019;MiltipleActiveResultSets=true;Encrypt=YES;TrustServerCertificate=YES"
+}
--- NET6-WEB-API/NET6.WEB/Program.cs
+++ NET6-WEB-API/NET6.WEB/Program.cs
@@ -1,8 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["Database:SqlServerConnection"]);
 
 var app = builder.Build();
 var configuration = app.Configuration;
@@ -42,68 +41,3 @@ if (app.Environment.IsStaging())
 }
 
 app.Run();
-
-public static class ProductRepository
-{
-    public static List<Product> Products { get; set; } = new List<Product>();
-
-    public static void Init(IConfiguration configuration) 
-    {
-        var products = configuration.GetSection("Products").Get<List<Product>>();
-        Products = products;
-    }
-
-    public static void Add(Product product) { Products.Add(product); }
-
-    public static Product GetBy(string code)
-    {
-        return Products.FirstOrDefault(x => x.Code == code);
-    }
-
-    public static void Remove(Product product)
-    {
-        Products.Remove(product);
-    }
-}
-
-public class Category 
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-}
-
-public class Product
-{
-    public int Id { get; set; }
-    public string Code { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public int CategoryId { get; set; }
-    public Category Category { get; set; }
-    public List<Tag> Tags { get; set; }
-}
-
-public class Tag 
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public int ProductId { get; set; }
-}
-public class ApplicationDbContext : DbContext 
-{
-    public DbSet<Product> Products { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Product>()
-            .Property(x => x.Description).HasMaxLength(500).IsRequired(false);
-        modelBuilder.Entity<Product>()
-            .Property(x => x.Name).HasMaxLength(120).IsRequired();
-        modelBuilder.Entity<Product>()
-            .Property(x => x.Code).HasMaxLength(20).IsRequired(false);
-    }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-PKH0UOM\\SQLEXPRESS;Initial Catalog=Products;Trusted_Connection=True; TrustServerCertificate=True;");
-    //"Server=localhost;Database=Products;User Id=sa;Password=@sql2019;MiltipleActiveResultSets=true;Encrypt=YES;TrustServerCertificate=YES"
-}
